06f4caef77e24af017cf3a50050401482d31b9ae
sinagtek-chrisjaypedra <chrisjay.pedra@xentragroup.com>
2026-06-02 01:39:34 +0800

    setup initialize

---
+++ src/assets/fonts/fonts.ts
@@ -0,0 +1,28 @@
+export const Fonts = {
+  // Asap Condensed Font Family
+  asapCondensed: {
+    black: 'AsapCondensed-Black',
+    blackItalic: 'AsapCondensed-BlackItalic',  
+    bold: 'AsapCondensed-Bold',
+    boldItalic: 'AsapCondensed-BoldItalic',
+    extraBold: 'AsapCondensed-ExtraBold',
+    extraBoldItalic: 'AsapCondensed-ExtraBoldItalic',
+    extraLight: 'AsapCondensed-ExtraLight',
+    extraLightItalic: 'AsapCondensed-ExtraLightItalic',
+    italic: 'AsapCondensed-Italic',
+    light: 'AsapCondensed-Light',
+    lightItalic: 'AsapCondensed-LightItalic',
+    medium: 'AsapCondensed-Medium',
+    mediumItalic: 'AsapCondensed-MediumItalic',
+    regular: 'AsapCondensed-Regular',
+    semibold: 'AsapCondensed-SemiBold',
+    semiboldItalic: 'AsapCondensed-SemiBoldItalic',
+    thin: 'AsapCondensed-Thin',
+    thinItalic: 'AsapCondensed-ThinItalic',
+  },
+  
+  // System Font
+  system: 'System',
+};
+
+export default Fonts;
